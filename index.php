edfc090aa3375ac37429ad755ff883c32eb6f888
LevelDrain <drain2291@gmail.com>
2021-03-29 18:53:38 +0900

    ロゴなどリソース追加

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>上島水準 Level Ueshima | オフィシャルサイト</title>
+    <title>上島水準 Level Ueshima | ポートフォリオ</title>
     <link rel="stylesheet" href="./shared/css/vender/lity.min.css">
     <link rel="stylesheet" href="./shared/css/vender/swiper-bundle.min.css">
     <link rel="stylesheet" href="./shared/css/common.css">
@@ -13,8 +13,61 @@
 
 <body>
     <div id="Container">
-        <div>
+        <div id="loading">
+            <div class="loader"></div>
         </div>
+
+        <header>
+            <h1 class="logo"><a href="/" title=""><img src="./shared/img/img_logo.png" alt=""></a></h1>
+            <a class="menu-btn sp-display">
+                <span></span>
+                <span></span>
+                <span></span>
+            </a>
+        </header>
+        <nav>
+            <div class="nav2row">
+                <ul class="nav-txt">
+                    <li><a href="/" itemprop="url">Top</a></li>
+                    <li><a href="/profile/" itemprop="url">Profile</a></li>
+                    <!-- <li><a href="/artworks/" itemprop="url">ArtWorks</a></li> -->
+                </ul>
+            </div>
+            <ul class="nav-sns">
+                <li><a href="" target="_blank" class="gc"><span class="icon-facebook"></span></a></li>
+                <li><a href="" target="_blank" class="gc"><span class="icon-twitter"></span></a></li>
+                <li><a href="" target="_blank" class="gc"><span class="icon-vimeo"></span></a></li>
+            </ul>
+        </nav>
+
+        <main>
+        <section id="TopImg">
+            <div class="p-top">
+
+            </div>
+        </section>
+        </main>
+
+        <footer>
+            <div class="fnav-sns">
+                <ul>
+                    <li><a href="" target="_blank" class="gc"><span class="icon-facebook"></span></a></li>
+                    <li><a href="" target="_blank" class="gc"><span class="icon-twitter"></span></a></li>
+                    <li><a href="" target="_blank" class="gc"><span class="icon-vimeo"></span></a></li>
+                </ul>
+            </div>
+
+            <div class="fnav">
+                <ul>
+                    <li><a href="/" itemprop="url">Top</a></li>
+                    <li><a href="/profile/" itemprop="url">Profile</a></li>
+                    <!-- <li><a href="/artworks/" itemprop="url">ArtWorks</a></li> -->
+                </ul>
+            </div>
+            <div id="copyrightArea">
+                <p id="copyright">© Level Ueshima</p>
+            </div>
+        </footer>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="./shared/js/vender/lazysizes.min.js"></script>
