c0094412eb4af07a8705132045df82cd054a4720
LevelDrain <drain2291@gmail.com>
2021-03-29 13:50:41 +0900

    環境再構築

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>上島水準 Level Ueshima | オフィシャルサイト</title>
+    <link rel="stylesheet" href="./shared/css/vender/lity.min.css">
+    <link rel="stylesheet" href="./shared/css/vender/swiper-bundle.min.css">
+    <link rel="stylesheet" href="./shared/css/common.css">
+</head>
+
+<body>
+    <div id="Container">
+        <div>
+        </div>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="./shared/js/vender/lazysizes.min.js"></script>
+    <script src="./shared/js/vender/lity.min.js"></script>
+    <script src="./shared/js/vender/swiper-bundle.min.js"></script>
+    <script src="./shared/js/index.js"></script>
+</body>
+
+</html>
