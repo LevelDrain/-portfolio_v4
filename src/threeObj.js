c7744f5a2559a2e5461c6c80900e611daa3a5009
LevelDrain <drain2291@gmail.com>
2021-04-09 15:44:21 +0900

    Three.jsアニメーションの修正

--- src/threeObj.js
+++ src/threeObj.js
@@ -3,11 +3,12 @@ import 'core-js'; // IEで動作するようにポリフィルを導入
 
 // threeObj.js
 let scene, camera, renderer, cloudParticles = [], flash;
+// 雨エフェクト一旦停止（配列操作実験中のため、処理落ちしたため）
+// let rain, rainGeo, vertices, rainCount = 5000;
 
 const drawThreejs = () => {
     const $canvas = document.querySelector('.l-bgThreejs'),
-        width = window.innerWidth,
-        height = window.innerHeight;
+        width = window.innerWidth, height = window.innerHeight;
 
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(60, width / height, 1, 2000);
@@ -20,20 +21,34 @@ const drawThreejs = () => {
     scene.add(ambient);
 
     let directionLight = new THREE.DirectionalLight(0xffffff);
-    directionLight.position.set([0, 0, 1]);
+    directionLight.position.set(0, 0, 1);
     scene.add(directionLight);
 
     flash = new THREE.PointLight(0x6b29a3, 30, 500, 1.7);
-    flash.position.set([200, 300, 100]);
+    flash.position.set(200, 300, 100);
     scene.add(flash);
-    
+
     renderer = new THREE.WebGLRenderer();
     scene.fog = new THREE.FogExp2(0x0b0125, 0.002);
     renderer.setClearColor(scene.fog.color);
-    $canvas.appendChild(renderer.domElement);
-
     renderer.setPixelRatio(window.devicePixelRatio);
     renderer.setSize(width, height);
+    $canvas.appendChild(renderer.domElement);
+
+    // rainGeo = new THREE.BufferGeometry();
+    // vertices = new Float32Array(rainCount);
+    // for (let i = 0; i < vertices.length; i += 3) {
+    //     vertices[i] = Math.random() * 400 - 200;
+    //     vertices[i + 1] = Math.random() * 500 - 250;
+    //     vertices[i + 2] = Math.random() * 400 - 200;
+    // }
+    // rainGeo.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+    //
+    // let rainMaterial = new THREE.PointsMaterial({
+    //     color: 0xaaaaaa, size: 0.1, transparent: true
+    // });
+    // rain = new THREE.Points(rainGeo, rainMaterial);
+    // scene.add(rain);
 
     let loader = new THREE.TextureLoader();
     loader.load('./img/smoke-1.png', (texture) => {
@@ -46,9 +61,9 @@ const drawThreejs = () => {
         for (let p = 0; p < 25; p++) {
             let cloud = new THREE.Mesh(cloudGeo, cloudMaterial);
             cloud.position.set(
-                Math.random() * 800 - 400,
+                Math.random() * 850 - 400,
                 500,
-                Math.random() * 500 - 450
+                Math.random() * 700 - 450
             );
             cloud.rotation.x = 1.16;
             cloud.rotation.y = -0.12;
@@ -66,19 +81,29 @@ const drawThreejs = () => {
 
 const animate = () => {
     cloudParticles.forEach(p => {
-        p.rotation.z = -0.002;
+        p.rotation.z -= 0.002;
     });
 
+    // let velocity = 0;
+    // velocity -= 1 + Math.random() * 0.1;
+    // rain.position.y += velocity;
+    //
+    // if (rain.position.y < -200) {
+    //     rain.position.y = 200;
+    //     velocity = 0;
+    // }
+
     if (Math.random() > 0.93 || flash.power > 100) {
-        if (flash.power > 100) {
+        if (flash.power < 100) {
             flash.position.set(
                 Math.random() * 400,
-                300 + Math.random() * 200,
+                300 + Math.random() * 500,
                 100
             );
         }
-        flash.power = 50 + Math.random() * 500;
+        flash.power = 50 + Math.random() * 400;
     }
+
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
 }
