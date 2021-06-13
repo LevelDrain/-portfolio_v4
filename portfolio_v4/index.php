dba16df932d47d596b3b8b3c1c7abf35d4c03b8b
LevelDrain <drain2291@gmail.com>
2021-06-14 01:53:49 +0900

    外部リンクへ飛ぶように修正

--- portfolio_v4/index.php
+++ portfolio_v4/index.php
@@ -5,9 +5,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="仕様だからと言わないアート系Webエンジニア「佐村木」のポートフォリオです">
-    <meta name="keywords" content="到龍門,とうりゅうもん,ポートフォリオ,フロントエンド,インタラクションデザイン,佐村木,上島水準">
-    <meta name="google-site-verification" content="tZqs0OHBtbjxrSPSumM4AMEh1bJaXhAhjaKFlA2_iIs" />
+    <meta name="description" content="デザインできるWebエンジニア「佐村木」のポートフォリオです">
+    <meta name="keywords" content="到龍門,とうりゅうもん,ポートフォリオ,フロントエンド,インタラクションデザイン,佐村木,上嶋レベル">
+    <meta name="google-site-verification" content="tZqs0OHBtbjxrSPSumM4AMEh1bJaXhAhjaKFlA2_iIs"/>
     <title>到龍門 - The road of Front End</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@300&display=swap" rel="stylesheet">
@@ -62,8 +62,12 @@
                             ?>
                             <?php if ($contents[6] == 'TRUE'): // pickup ?>
                             <div class="swiper-slide slide-item">
-                                <a class="animsition-link" href="./articles/<?= $contents[4] ?>">
-                                    <img src="articles/img/<?= $contents[1] ?>" alt="">
+                                <?php if (substr($contents[4], 0, 6) === 'https:') { // 外部リンクの場合
+                                    echo '<a href="' . $contents[4] . '" target="_blank" rel="noopener noreferrer">';
+                                } else {
+                                    echo '<a class="animsition-link" href="./articles/' . $contents[4] . '">';
+                                } ?>
+                                <img src="articles/img/<?= $contents[1] ?>" alt="" style="pointer-events: auto">
                                 </a>
                             </div>
                         <?php endif; ?>
@@ -135,17 +139,21 @@
                     </div>
 
                     <div class="c-accordion--parent">
-                        <h2 class="c-title">システム開発系</h2>
+                        <h2 class="c-title">職務経歴コンテンツ</h2>
                     </div>
 
                     <div class="c-accordion--child">
                         <div class="l-flex--pc">
                             <!-- ここからカード展開 -->
-                            <?php
-                            foreach ($record as $id => $contents):
-                                ?>
-                                <?php if ($contents[5] == 'system'): // カテゴリ ?>
-                                <a class="c-card animsition-link" href="./articles/<?= $contents[4] ?>">
+                            <?php foreach ($record as $id => $contents): ?>
+
+                                <?php if ($contents[5] == 'career'): // カテゴリ  ?>
+
+                                    <?php if (substr($contents[4], 0, 6) === 'https:') { // 外部リンクの場合
+                                        echo '<a class="c-card" href="' . $contents[4] . '" target="_blank" rel="noopener noreferrer">';
+                                    } else {
+                                        echo '<a class="c-card animsition-link" href="./articles/' . $contents[4] . '">';
+                                    } ?>
                                     <img class="c-card--img" src="articles/img/<?= $contents[1] ?>" alt="">
                                     <div class="c-card--body">
                                         <h5 class="c-card--title"><?= $contents[2] ?></h5>
@@ -153,8 +161,9 @@
                                             <?= $contents[3] ?>
                                         </p>
                                     </div>
-                                </a>
-                            <?php endif; ?>
+                                    </a>
+
+                                <?php endif; ?>
 
                             <?php endforeach; ?>
                             <!-- カード終了 -->
@@ -202,7 +211,8 @@
                     </div>
                     <div class="text">
                         <p class="c-heading heading--pc">
-                            フロントエンド開発からデザイン、コピーライティングまで手掛けるWeb制作のマルチポテンシャライト。また、上嶋レベル（Level Ueshima）として、漫画が描けるデザイナーとしての活動を行う。
+                            フロントエンド開発からデザイン、コピーライティングまで手掛けるWeb制作のマルチポテンシャライト。また、上嶋レベル（Level
+                            Ueshima）として、漫画が描けるデザイナーとしての活動を行う。
                         </p>
                         <p class="c-heading">
                             京都コンピュータ学院にてJavaScript講師に根性を叩きなおされ、不平不満だらけの人生を変えるべく師事した経緯がある。
