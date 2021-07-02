bb6e8e90f7dfaa0e11b40093dc48a3816e7cd7fb
LevelDrain <drain2291@gmail.com>
2021-07-03 02:05:31 +0900

    ページ8追加

--- portfolio_v4/articles/8/index.php
+++ portfolio_v4/articles/8/index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>I社様予約システム | 到龍門</title>
+    <title>学生時代ポートフォリオ | 到龍門</title>
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Serif+JP:wght@300&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./../../shared/css/vendor/lity.min.css">
@@ -26,24 +26,77 @@
     <main class="l-main">
         <section class="p-article--img">
             <div class="l-container">
-                <img src="../img/img_system.png" alt="">
+                <img src="../img/img_portfolio1.png" alt="">
             </div>
         </section>
 
         <section class="p-article--text">
             <div class="l-container section-fadein">
-                <h2 class="c-title">I社様予約システム</h2>
+                <h2 class="c-title">学生時代ポートフォリオ</h2>
                 <p class="c-heading">
-                    管理画面のGUIの修正および新規実装を担当。
+                    「理性と野生」という化け猫の野生感と理知的な内容を併せ持った世界観をテーマにした学生作品。<br>
+                    内容は学生視点のところが多く、今から思えば反省点が多い。
                 </p>
                 <p class="c-heading">開発環境</p>
                 <ul class="p-article--list">
-                    <li>Vue.js</li>
-                    <li>Laravel</li>
+                    <li>HTML / CSS</li>
+                    <li>CLIP STUDIO PAINT</li>
+                    <li>csshake</li>
                 </ul>
-                <div class="p-article--btnbox c-border-top">
-                    <a href="javascript:history.back();" class="c-button">戻る</a>
+            </div>
+
+            <div class="l-container section-fadein">
+                <h3 class="p-article--title">描き下ろし画像一覧</h3>
+                <p class="c-heading">
+                    ポートフォリオ制作にあたり、特別に「化け猫」をテーマにしたイラストを描き下ろした。灰色の猫はページ削減のため未使用。
+                </p>
+                <div class="p-article--fig wl">
+                    <img src="../img/p1_bg_toppage.jpg" alt="">
                 </div>
+                <div class="p-article--fig wl">
+                    <img src="../img/p1_bg_works.jpg" alt="">
+                </div>
+                <div class="p-article--fig wl">
+                    <img src="../img/p1_bg_about.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="l-container section-fadein">
+                <h3 class="p-article--title">スマホ用画像一覧</h3>
+                <p class="c-heading">
+                    レスポンシブ対応用。
+                </p>
+                <div class="p-article--fig centering">
+                    <img src="../img/p1_bg_toppage_s.jpg" alt="">
+                </div>
+                <div class="p-article--fig centering">
+                    <img src="../img/p1_bg_works_s.jpg" alt="">
+                </div>
+                <div class="p-article--fig centering">
+                    <img src="../img/p1_bg_about_s.jpg" alt="">
+                </div>
+            </div>
+
+            <div class="l-container section-fadein">
+                <h3 class="p-article--title">ページトップへボタン</h3>
+                <p class="c-heading">
+                    「理性と野生」ポートフォリオ上ではブルブル震えるcsshakeによるアニメーションつき。<br>
+                    <a href="https://elrumordelaluz.github.io/csshake/" target="_blank"
+                       rel="noopener noreferrer">☞ csshake 外部リンク</a>
+                </p>
+
+                <div class="p-article--fig centering">
+                    <img src="../img/p1_img_to_top.png" alt="">
+                </div>
+            </div>
+
+            <div class="p-article--btnbox">
+                <a href="https://leveldrain.sumomo.ne.jp/Portfolio/" class="c-button" target="_blank"
+                   rel="noopener noreferrer">実際のWebサイトへ</a>
+            </div>
+
+            <div class="p-article--btnbox c-border-top">
+                <a href="javascript:history.back();" class="c-button">戻る</a>
             </div>
         </section>
     </main>
